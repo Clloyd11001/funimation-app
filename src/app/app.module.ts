b62ae88e43cc07401b8d310da65177803d08b472
Collin Lloyd <91685082+Clloyd11001@users.noreply.github.com>
2022-09-27 20:38:18 -0400

    Lab 3
    Implemented cards and began slideshow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FirNavBarComponent } from './navigation/fir-nav-bar/fir-nav-bar.component';
 import { AlarmComponent } from './alarm/alarm.component';
 import { BodyComponent } from './body/body.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirNavBarComponent,
     AlarmComponent,
-    BodyComponent
+    BodyComponent,
+    SlideshowComponent
   ],
   imports: [
     BrowserModule,
